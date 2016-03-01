78fed58289fed5ecd5b03b2927a40208e702566f
iorobertob <io_robertob@hotmail.com>
2016-03-01 10:21:10 -0800

    Test for Branch
    Test for branching

--- app/src/main/java/com/patterns/io/thingsflow/DevicesFragment.java
+++ app/src/main/java/com/patterns/io/thingsflow/DevicesFragment.java
@@ -40,7 +40,8 @@ public class DevicesFragment extends Fragment {
                 "Device 1",
                 "Device 2",
                 "Device 3",
-                "Test for GitHub"
+                "Test for GitHub",
+                "Test for Branch"
         };
 
         List<String> devices = new ArrayList<String>(Arrays.asList(arrayStrings));
