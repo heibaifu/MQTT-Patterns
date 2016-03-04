4e4041bc2dca4988b71395636ddfbbbeca3728b7
iorobertob <io_robertob@hotmail.com>
2016-03-04 12:21:29 -0800

    Added Menu and reconfig AsyncTask
    Added Menu through a separate xml, and re structured the AsynTask call,
    and parameters

--- app/src/main/java/com/patterns/io/thingsflow/DevicesFragment.java
+++ app/src/main/java/com/patterns/io/thingsflow/DevicesFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -31,10 +34,44 @@ public class DevicesFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.devicefragment, menu);
+        //getMenuInflater().inflate(R.menu.devicefragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            FetchDevicesTask devicesTask = new FetchDevicesTask();
+            devicesTask.execute();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
 
         String[] arrayStrings = {
                 "Device 1",
@@ -64,17 +101,17 @@ public class DevicesFragment extends Fragment {
         // Bind the adapter to the List View
         listView.setAdapter(devicesAdapter);
 
-        new FetchDevicesTask().execute("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+        //new FetchDevicesTask().execute("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7&appid=8889689cdc229214c182a2ea6a3f5e55");
 
 
         return rootView;
 
     }
 
-    public class FetchDevicesTask extends AsyncTask <String, Void, String>{
+    public class FetchDevicesTask extends AsyncTask <Void, Void, Void>{
 
         @Override
-        protected String doInBackground(String... urlString){
+        protected Void doInBackground(Void... params) {
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
@@ -84,10 +121,11 @@ public class DevicesFragment extends Fragment {
             String forecastJsonStr = null;
 
             try {
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7&appid=8889689cdc229214c182a2ea6a3f5e55";
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL(urlString[0]);
+                URL url = new URL(baseUrl);
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -121,7 +159,7 @@ public class DevicesFragment extends Fragment {
                 // If the code didn't successfully get the weather data, there's no point in attemping
                 // to parse it.
                 return null;
-            } finally{
+            } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
                 }
@@ -133,7 +171,7 @@ public class DevicesFragment extends Fragment {
                     }
                 }
             }
-            return forecastJsonStr;
+            return null;
         }
 
 
