a969b65332ccc3d5cc64e55036018a15559ac73c
iorobertob <io_robertob@hotmail.com>
2016-03-09 16:44:12 -0800

    Included Refreseh option in menu
    Copied the Refresh inflate funciton from the DevicesFragment.java, with
    the same xml used

--- app/src/main/java/com/patterns/io/thingsflow/DetailActivityFragment.java
+++ app/src/main/java/com/patterns/io/thingsflow/DetailActivityFragment.java
@@ -1,8 +1,10 @@
 package com.patterns.io.thingsflow;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,8 +17,24 @@ public class DetailActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.devicefragment, menu);
+        //getMenuInflater().inflate(R.menu.devicefragment, menu);
+    }
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_detail, container, false);
     }
+
 }
