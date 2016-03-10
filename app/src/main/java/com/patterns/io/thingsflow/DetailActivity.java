2595c794daa755560ae325d8962f70ca886d54ce
iorobertob <io_robertob@hotmail.com>
2016-03-10 14:22:33 -0800

    commit

--- app/src/main/java/com/patterns/io/thingsflow/DetailActivity.java
+++ app/src/main/java/com/patterns/io/thingsflow/DetailActivity.java
@@ -17,7 +17,6 @@ public class DetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
